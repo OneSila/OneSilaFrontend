389118b7582dd8e4073bd8323fe1b3a7e7aa014e
BranDavidSebastian <brandavid1998@gmail.com>
2024-03-13 17:02:33 +0200

    add auto deployment script

--- scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mv frontend frontend_old
-mkdir frontend
-mv dist.zip frontend
-cd frontend
-unzip dist.zip
-rm dist.zip
-cd ..
-rm -rf frontend_old
--- scripts/init.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-echo "Install NPM packages."
-npm i
-echo "Run DEV server."
-npm run dev -- --host
