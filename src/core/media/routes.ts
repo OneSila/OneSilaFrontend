1d21ac1664701f4c817d59690d6ba0b53d3afd70
BranDavidSebastian <brandavid1998@gmail.com>
2024-05-29 23:33:49 +0300

    temporary change the name to medias

--- src/core/media/routes.ts
+++ src/core/media/routes.ts
@@ -1,10 +1,10 @@
 export const routes = [
-  { path: '/media/recent-files', name: 'media.files', component: () => import('./files/FilesListController.vue') },
-  { path: '/media/images', name: 'media.images.list', component: () => import('./images/images-list/ImagesListController.vue') },
-  { path: '/media/videos', name: 'media.videos.list', component: () => import('./videos/videos-list/VideosListController.vue') },
-  { path: '/media/documents', name: 'media.documents.list', component: () => import('./documents/documents-list/DocumentsListController.vue') },
+  { path: '/medias/files', name: 'media.files', component: () => import('./files/FilesListController.vue') },
+  { path: '/medias/images', name: 'media.images.list', component: () => import('./images/images-list/ImagesListController.vue') },
+  { path: '/medias/videos', name: 'media.videos.list', component: () => import('./videos/videos-list/VideosListController.vue') },
+  { path: '/medias/documents', name: 'media.documents.list', component: () => import('./documents/documents-list/DocumentsListController.vue') },
 
-  { path: '/media/images/:id', name: 'media.images.show', component: () => import('./images/image-show/ImageShowController.vue') },
-  { path: '/media/videos/:id', name: 'media.videos.show', component: () => import('./videos/video-show/VideoShowController.vue') },
-  { path: '/media/documents/:id', name: 'media.documents.show', component: () => import('./documents/document-show/DocumentShowController.vue') },
+  { path: '/medias/images/:id', name: 'media.images.show', component: () => import('./images/image-show/ImageShowController.vue') },
+  { path: '/medias/videos/:id', name: 'media.videos.show', component: () => import('./videos/video-show/VideoShowController.vue') },
+  { path: '/medias/documents/:id', name: 'media.documents.show', component: () => import('./documents/document-show/DocumentShowController.vue') },
 ]
