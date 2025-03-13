2a927eb4264e0e0ff7bbf4a5eccf15380f69cdb5
david <brandavid1998@gmail.com>
2025-03-13 23:32:44 +0200

    #320 hotfix broken parent status

--- src/shared/api/queries/products.js
+++ src/shared/api/queries/products.js
@@ -157,6 +157,7 @@ export const configurableVariationsQuery = gql`
             id
             sku
             name
+            active
             thumbnailUrl
             inspectorStatus
           }
