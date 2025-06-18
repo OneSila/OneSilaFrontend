564d175655c4b691d0d7ceb712924085ec5b4d37
David Sebastian Bran <50082813+BranDavidSebastian@users.noreply.github.com>
2025-06-18 17:41:09 +0300

    feat: add mapping wizard

--- src/core/integrations/integrations/integrations-show/containers/properties/containers/amazon-properties/containers/IntegrationsAmazonPropertyEditController.vue
+++ src/core/integrations/integrations/integrations-show/containers/properties/containers/amazon-properties/containers/IntegrationsAmazonPropertyEditController.vue
@@ -1,20 +1,62 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { useI18n } from 'vue-i18n';
+import { useRouter } from 'vue-router';
 import GeneralTemplate from "../../../../../../../../../shared/templates/GeneralTemplate.vue";
 import { Breadcrumbs } from "../../../../../../../../../shared/components/molecules/breadcrumbs";
 import { GeneralForm } from "../../../../../../../../../shared/components/organisms/general-form";
 import { useRoute } from "vue-router";
-import { amazonPropertyEditFormConfigConstructor } from "../configs";
+import { amazonPropertyEditFormConfigConstructor, listingQuery } from "../configs";
+import apolloClient from "../../../../../../../../../../apollo-client";
+import { Toast } from "../../../../../../../../../shared/modules/toast";
 
 const { t } = useI18n();
 const route = useRoute();
+const router = useRouter();
 
 const propertyId = ref(String(route.params.id));
 const type = ref(String(route.params.type));
 const integrationId = route.query.integrationId ? route.query.integrationId.toString() : '';
+const salesChannelId = route.query.salesChannelId ? route.query.salesChannelId.toString() : '';
+const isWizard = route.query.wizard === '1';
 
 const formConfig = amazonPropertyEditFormConfigConstructor(t, type.value, propertyId.value, integrationId);
+
+if (isWizard) {
+  formConfig.submitUrl = undefined;
+  formConfig.submitLabel = t('integrations.show.mapping.saveAndMapNext');
+}
+
+const fetchNextUnmapped = async () => {
+  const { data } = await apolloClient.query({
+    query: listingQuery,
+    variables: {
+      first: 1,
+      filter: {
+        salesChannel: { id: { exact: salesChannelId } },
+        mappedLocally: { exact: false },
+      },
+    },
+    fetchPolicy: 'network-only',
+  });
+  const edges = data?.amazonProperties?.edges || [];
+  return edges.length > 0 ? edges[0].node.id : null;
+};
+
+const handleSubmit = async () => {
+  if (!isWizard) return;
+  const nextId = await fetchNextUnmapped();
+  if (nextId) {
+    router.replace({
+      name: 'integrations.amazonProperties.edit',
+      params: { type: type.value, id: nextId },
+      query: { integrationId, salesChannelId, wizard: '1' },
+    });
+  } else {
+    Toast.success(t('integrations.show.mapping.allMappedSuccess'));
+    router.push({ name: 'integrations.integrations.show', params: { type: type.value, id: integrationId }, query: { tab: 'properties' } });
+  }
+};
 </script>
 
 <template>
@@ -27,7 +69,7 @@ const formConfig = amazonPropertyEditFormConfigConstructor(t, type.value, proper
         ]" />
     </template>
     <template v-slot:content>
-      <GeneralForm :config="formConfig" />
+      <GeneralForm :config="formConfig" @submit="handleSubmit" />
     </template>
   </GeneralTemplate>
 </template>
--- src/core/integrations/integrations/integrations-show/containers/rules/containers/amazon-product-types/containers/IntegrationsAmazonProductTypeEditController.vue
+++ src/core/integrations/integrations/integrations-show/containers/rules/containers/amazon-product-types/containers/IntegrationsAmazonProductTypeEditController.vue
@@ -1,25 +1,66 @@
 <script setup lang="ts">
 import {onMounted, ref} from 'vue';
+import { useRouter } from 'vue-router';
 import { useI18n } from 'vue-i18n';
 import GeneralTemplate from "../../../../../../../../../shared/templates/GeneralTemplate.vue";
 import { Breadcrumbs } from "../../../../../../../../../shared/components/molecules/breadcrumbs";
 import { GeneralForm } from "../../../../../../../../../shared/components/organisms/general-form";
 import { useRoute } from "vue-router";
-import { amazonProductTypeEditFormConfigConstructor } from "../configs";
+import { amazonProductTypeEditFormConfigConstructor, listingQuery } from "../configs";
 import apolloClient from "../../../../../../../../../../apollo-client";
 import {propertiesQuery} from "../../../../../../../../../shared/api/queries/properties";
 import {Link} from "../../../../../../../../../shared/components/atoms/link";
 import {Button} from "../../../../../../../../../shared/components/atoms/button";
+import { Toast } from "../../../../../../../../../shared/modules/toast";
 
 const { t } = useI18n();
 const route = useRoute();
+const router = useRouter();
 
 const productTypeId = ref(String(route.params.id));
 const type = ref(String(route.params.type));
 const integrationId = route.query.integrationId ? route.query.integrationId.toString() : '';
+const salesChannelId = route.query.salesChannelId ? route.query.salesChannelId.toString() : '';
+const isWizard = route.query.wizard === '1';
 const propertyProductTypeId = ref<string | null>(null);
 
 const formConfig = amazonProductTypeEditFormConfigConstructor(t, type.value, productTypeId.value, integrationId);
+
+if (isWizard) {
+  formConfig.submitUrl = undefined;
+  formConfig.submitLabel = t('integrations.show.mapping.saveAndMapNext');
+}
+
+const fetchNextUnmapped = async () => {
+  const { data } = await apolloClient.query({
+    query: listingQuery,
+    variables: {
+      first: 1,
+      filter: {
+        salesChannel: { id: { exact: salesChannelId } },
+        mappedLocally: { exact: false },
+      },
+    },
+    fetchPolicy: 'network-only',
+  });
+  const edges = data?.amazonProductTypes?.edges || [];
+  return edges.length > 0 ? edges[0].node.id : null;
+};
+
+const handleSubmit = async () => {
+  if (!isWizard) return;
+  const nextId = await fetchNextUnmapped();
+  if (nextId) {
+    router.replace({
+      name: 'integrations.amazonProductTypes.edit',
+      params: { type: type.value, id: nextId },
+      query: { integrationId, salesChannelId, wizard: '1' },
+    });
+  } else {
+    Toast.success(t('integrations.show.mapping.allMappedSuccess'));
+    router.push({ name: 'integrations.integrations.show', params: { type: type.value, id: integrationId }, query: { tab: 'productRules' } });
+  }
+};
 
 const fetchProductType = async () => {
     const {data} = await apolloClient.query({
@@ -57,7 +98,7 @@ onMounted(fetchProductType);
     </template>
 
     <template v-slot:content>
-      <GeneralForm :config="formConfig" />
+      <GeneralForm :config="formConfig" @submit="handleSubmit" />
     </template>
   </GeneralTemplate>
 </template>
