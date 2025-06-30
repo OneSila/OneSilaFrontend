79b07726e2b959505650eb98fd810490940dfa48
david <brandavid1998@gmail.com>
2025-06-30 14:20:08 +0300

    #425 hotfix little issue

--- src/core/integrations/integrations/integrations-show/containers/rules/containers/amazon-product-types/AmazonProductTypes.vue
+++ src/core/integrations/integrations/integrations-show/containers/rules/containers/amazon-product-types/AmazonProductTypes.vue
@@ -51,7 +51,7 @@ const listingConfig = amazonProductTypesListingConfigConstructor(t, props.id);
 
 <template>
   <GeneralTemplate>
-    <template v-slot:buttons>s
+    <template v-slot:buttons>
       <Button type="button" class="btn btn-secondary" :disabled="!canStartMapping" @click="startMapping">
         {{ t('integrations.show.mapping.startMapping') }}
       </Button>
