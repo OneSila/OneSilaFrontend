24b53ab033dd97291cfcfd15d5e50d1bc9cf47da
David Sebastian Bran <50082813+BranDavidSebastian@users.noreply.github.com>
2025-08-26 16:17:29 +0300

    Fix variation selector layout and search

--- src/core/products/products/product-show/containers/tabs/variations/containers/variation-create/CreateForm.vue
+++ src/core/products/products/product-show/containers/tabs/variations/containers/variation-create/CreateForm.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 
 import { ref, onMounted, watch } from 'vue';
+import debounce from 'lodash.debounce';
 import { Product } from "../../../../../../configs";
 import { useI18n } from "vue-i18n";
 import { ProductType, variationTypes } from "../../../../../../../../../shared/utils/constants";
@@ -15,6 +16,7 @@ const { t } = useI18n();
 const props = defineProps<{ product: Product, form: VariationForm, variationIds: string[] }>();
 const variations = ref([]);
 const loading = ref(false);
+const search = ref('');
 
 const cleanedData = (rawData) => {
   return rawData?.edges ? rawData.edges.map(edge => edge.node) : rawData;
@@ -39,18 +41,20 @@ const fetchData = async () => {
       typeFilter = variationTypes;
   }
 
+  const filter: any = {
+    NOT: { id: { inList: props.variationIds } },
+    type: { inList: typeFilter },
+  };
+
+  if (search.value) {
+    filter.search = search.value;
+  }
 
   const { data } = await apolloClient.query({
     query: productsQuery,
-    variables: {
-      filter: {
-        NOT: { id: { inList: props.variationIds } },
-        type: { inList: typeFilter },
-      },
-    },
+    variables: { filter },
     fetchPolicy: 'network-only',
   });
-
 
   if (data) {
     variations.value = cleanedData(data.products);
@@ -59,6 +63,11 @@ const fetchData = async () => {
   loading.value = false;
 
 };
+
+const handleInput = debounce((value: string) => {
+  search.value = value;
+  fetchData();
+}, 500);
 
 onMounted(fetchData);
 
@@ -74,7 +83,7 @@ watch(() => props.variationIds, fetchData, { deep: true });
     </svg>
   </Flex>
   <Flex v-else gap="2">
-    <FlexCell>
+    <FlexCell grow>
       <Selector v-if="variations.length > 0"
                 v-model="form.variation"
                 :options="variations"
@@ -84,7 +93,8 @@ watch(() => props.variationIds, fetchData, { deep: true });
                 :removable="false"
                 :placeholder="t('shared.placeholders.product')"
                 filterable
-                class="min-w-[200px] mr-2" />
+                class="mr-2 w-full"
+                @searched="handleInput" />
     </FlexCell>
     <FlexCell v-if="product.type !== ProductType.Configurable">
       <TextInput v-model="form.quantity" float :placeholder="t('shared.placeholders.quantity')" class="w-32" />
