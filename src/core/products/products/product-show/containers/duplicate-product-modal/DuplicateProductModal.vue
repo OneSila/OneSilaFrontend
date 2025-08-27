90b396f2c7caaa8551c18050c797c252c146e4ab
David Sebastian Bran <50082813+BranDavidSebastian@users.noreply.github.com>
2025-08-27 13:52:08 +0300

    use toggle for alias option

--- src/core/products/products/product-show/containers/duplicate-product-modal/DuplicateProductModal.vue
+++ src/core/products/products/product-show/containers/duplicate-product-modal/DuplicateProductModal.vue
@@ -5,7 +5,7 @@ import { Card } from "../../../../../../shared/components/atoms/card";
 import { Modal } from "../../../../../../shared/components/atoms/modal";
 import { TextInput } from "../../../../../../shared/components/atoms/input-text";
 import { Button } from "../../../../../../shared/components/atoms/button";
-import { Checkbox } from "../../../../../../shared/components/atoms/checkbox";
+import { Toggle } from "../../../../../../shared/components/atoms/toggle";
 
 const props = defineProps<{ modelValue: boolean; isConfigurable: boolean }>();
 const emit = defineEmits<{
@@ -44,9 +44,9 @@ const submit = () => {
         <h3 class="text-xl font-semibold text-center mb-4">{{ t('shared.button.duplicate') }}</h3>
         <p class="mb-4">{{ t('products.products.duplicateModal.description') }}</p>
         <TextInput v-model="sku" :placeholder="t('products.products.labels.sku')" class="w-full" />
-        <Checkbox v-if="!props.isConfigurable" v-model="createAsAlias" class="w-full mt-4">
+        <Toggle v-if="!props.isConfigurable" v-model="createAsAlias" class="w-full mt-4">
           {{ t('products.products.duplicateModal.createAsAlias') }}
-        </Checkbox>
+        </Toggle>
         <div class="flex justify-end gap-4 mt-4">
           <Button class="btn btn-outline-dark" @click="closeModal">{{ t('shared.button.cancel') }}</Button>
           <Button class="btn btn-primary" @click="submit">{{ t('shared.button.duplicate') }}</Button>
