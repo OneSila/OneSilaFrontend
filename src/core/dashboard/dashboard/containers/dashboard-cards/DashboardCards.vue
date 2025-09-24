ab1ff9ff051df9cbd8b1caf35db73ae0d2d2889c
David Sebastian Bran <50082813+BranDavidSebastian@users.noreply.github.com>
2025-09-24 22:12:42 +0300

    Add eBay dashboard cards and fix Amazon toggle state

--- src/core/dashboard/dashboard/containers/dashboard-cards/DashboardCards.vue
+++ src/core/dashboard/dashboard/containers/dashboard-cards/DashboardCards.vue
@@ -1,18 +1,23 @@
 <script lang="ts" setup>
-
+import { computed } from 'vue';
 import { DashboardSectionProducts } from "./containers/dashboard-section-products";
 import { DashboardSectionGeneral } from "./containers/dashboard-section-general";
 import { DashboardSectionAmazon } from "./containers/dashboard-section-amazon";
+import { DashboardSectionEbay } from "./containers/dashboard-section-ebay";
 import { injectAuth } from "../../../../../shared/modules/auth";
+
 const auth = injectAuth();
 
+const hasAmazonIntegration = computed(() => Boolean(auth.user.company?.hasAmazonIntegration));
+const hasEbayIntegration = computed(() => Boolean((auth.user.company as any)?.hasEbayIntegration));
 </script>
 
 <template>
   <div>
     <DashboardSectionProducts />
     <DashboardSectionGeneral />
-    <DashboardSectionAmazon v-if="auth.user.company?.hasAmazonIntegration" />
+    <DashboardSectionAmazon v-if="hasAmazonIntegration" />
+    <DashboardSectionEbay v-if="hasEbayIntegration" />
   </div>
 </template>
 
