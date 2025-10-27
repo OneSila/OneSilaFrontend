5d330d9389484d7ac82662fc924eeb524a4493ac
david <brandavid1998@gmail.com>
2025-10-27 17:42:56 +0200

    fix ebay filters

--- src/core/integrations/integrations/integrations-show/containers/rules/containers/remote-product-types/configs.ts
+++ src/core/integrations/integrations/integrations-show/containers/rules/containers/remote-product-types/configs.ts
@@ -170,7 +170,6 @@ export const productTypesSearchConfigConstructor = (
       addLookup: true,
       lookupKeys: ['id'],
       queryVariables: { filters: { salesChannel: { id: { exact: salesChannelId } } } },
-      default: salesChannelId,
     });
   }
 
